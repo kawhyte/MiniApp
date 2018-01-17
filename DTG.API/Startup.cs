d1055567779f48fce871ed776c05a9523e931f0a
kawhyte <kawhyte@gmail.com>
2018-01-17 16:36:46 -0500

    Adde Global Error Handling (Server Side)

--- DTG.API/Startup.cs
+++ DTG.API/Startup.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using DTG.API.Data;
+using DTG.API.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -54,6 +58,25 @@ namespace DTG.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+            app.UseExceptionHandler(builder => {
+
+                builder.Run(async context => {
+                    context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                    var error = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if(error != null)
+                    {
+                        context.Response.AddApplicatinError(error.Error.Message);
+                        await context.Response.WriteAsync( error.Error.Message);
+                    }
+                });
+            });
+
+                
             }
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseAuthentication();
