b0782d77e16b91a9d277d1bd76d0f97231f69c61
Kenny Whyte <K@gmail.com>
2018-07-02 11:01:43 -0400

    Client side filtering

--- DTG.SPA/src/app/vehicle-list/vehicle-list.component.ts
+++ DTG.SPA/src/app/vehicle-list/vehicle-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Vehicle } from '../_models/vehicle';
+import { Vehicle, KeyValuePair } from '../_models/vehicle';
 import { VehicleService } from '../_services/vehicle.service';
 
 @Component({
@@ -10,12 +10,35 @@ import { VehicleService } from '../_services/vehicle.service';
 export class VehicleListComponent implements OnInit {
 
   vehicles: Vehicle[];
+  allVehicles: Vehicle[];
+  makes: KeyValuePair[];
+  filter: any = {};
 
   constructor(private vehicleService: VehicleService) { }
 
   ngOnInit() { 
     this.vehicleService.getVehicles()
-      .subscribe(vehicles => this.vehicles = vehicles);
+      //.subscribe(vehicles => this.vehicles = vehicles)
+      .subscribe(vehicles => this.vehicles = this.allVehicles = vehicles);
+
+      this.vehicleService.getMakes()
+           .subscribe(makes => this.makes = makes);
   }
 
+  onFilterChange() {
+    var vehicles = this.allVehicles;
+    
+        if (this.filter.makeId)
+          vehicles = vehicles.filter(v => v.make.id == this.filter.makeId);
+    
+        if (this.filter.modelId)
+          vehicles = vehicles.filter(v => v.model.id == this.filter.modelId);
+    
+        this.vehicles = vehicles;
+
+  }
+   resetFilter() {
+      this.filter = {};
+      this.onFilterChange();
+   }
 }
